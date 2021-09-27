c71bb914fd54be0d2de2fb6d5f667448e33be1d1
Helena <Helena322@yandex.ru>
2021-09-28 02:07:56 +0400

    Работа над ошибками5

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
-const swap = function (min, max) {
-  return min > max ? [max, min] : [min, max];
-};
-
-const arrWithNumbers = function (from, to) {
-  const [min, max] = swap (from, to);
-  return Math.random() * (max - min) + min;
-};
-
-const coordinates = function (from, to, point) {
-  return (from < to) ? parseFloat((Math.random() * (to - from) + from).toFixed(point)) : parseFloat(arrWithNumbers(from, to).toFixed(point));
+const getRandomFloatRange = function (from, to, point) {
+  const min = Math.min(from, to);
+  const max = Math.max(from, to);
+  return parseFloat((Math.random() * (max - min) + min).toFixed(point));
 };
 
 const getRandomIntFromRange = function (min, max) {
-  return Math.round(coordinates(min, max));
+  return Math.round(getRandomFloatRange(min, max));
 };
 
 getRandomIntFromRange(0, 10);
 
-coordinates(100, 200, 2);
+getRandomFloatRange(100, 200, 2);
 
 //Использовались материалы https://learn.javascript.ru/number
