b6ffb9105f83efcca9337bfd89ded85e8912d26b
Helena <Helena322@yandex.ru>
2021-10-16 14:32:42 +0400

    Отрисовка 2

--- js/form.js
+++ js/form.js
@@ -1,38 +1,41 @@
-export const form = document.querySelector('.ad-form');
-export const formElement = document.querySelector('.ad-form').querySelector('fieldset');
-export const formElements = document.querySelector('.ad-form').querySelectorAll('fieldset');
+const form = document.querySelector('.ad-form');
+let formElement = form.querySelector('fieldset');
+const formElements = form.querySelectorAll('fieldset');
 
-export const mapFilters = document.querySelector('.map__filters');
-export const mapFiltersElement = document.querySelector('.map__filters').querySelector('select');
-export const mapFiltersElements = document.querySelector('.map__filters').querySelectorAll('select');
-export const mapFiltersFieldset = document.querySelector('.map__filters').querySelector('fieldset');
+const mapFilters = document.querySelector('.map__filters');
+let mapFiltersElement = mapFilters.querySelector('select');
+const mapFiltersElements = mapFilters.querySelectorAll('select');
+const mapFiltersFieldset = mapFilters.querySelector('fieldset');
 
-export const t1 = () => {
+export const getFormDisabled = () => {
   form.classList.add('ad-form--disabled');
   mapFilters.classList.add('map__filters--disabled');
 
-  for (let formElement of formElements) {
+  for (formElement of formElements) {
     formElement.classList.add('disabled');
   }
 
-  for (let mapFiltersElement of mapFiltersElements) {
+  for (mapFiltersElement of mapFiltersElements) {
     mapFiltersElement.classList.add('disabled');
   }
 
   mapFiltersFieldset.classList.add('disabled');
 };
 
-export const t2 = () => {
+export const getFormEnabled = () => {
   form.classList.remove('ad-form--disabled');
   mapFilters.classList.remove('map__filters--disabled');
 
-  for (let formElement of formElements) {
+  for (formElement of formElements) {
     formElement.classList.remove('disabled');
   }
 
-  for (let mapFiltersElement of mapFiltersElements) {
+  for (mapFiltersElement of mapFiltersElements) {
     mapFiltersElement.classList.remove('disabled');
   }
 
   mapFiltersFieldset.classList.remove('disabled');
 };
+
+getFormDisabled();
+getFormEnabled();
