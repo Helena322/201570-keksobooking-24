2ba328f1129a85f53c7bcae5a9dbcb636e736d97
Helena <Helena322@yandex.ru>
2021-11-17 00:16:20 +0400

    Правки2

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ import {createMap, showData} from './map.js';
 import {getData} from './request.js';
 import {initFilter} from './filter.js';
 
-const getMapLoad = () => {
+const getAblePage = () => {
   getData((data) => {
     initFilter(data);
     showData(data.slice(0, SIMILAR_DATA_COUNT));
   });
 };
 
-createMap(getMapLoad);
+createMap(getAblePage);
